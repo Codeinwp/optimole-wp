d6fd9c90de4dce2b1e7e8a297148748d60bb3338
selul <marius.cristea@vertistudio.com>
2019-01-07 19:35:25 +0200

    build: ignore code coverage on non cc environment

--- bin/run-unit-tests.sh
+++ bin/run-unit-tests.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-composer global require "phpunit/phpunit=4.8.*|5.7.*"
-bash bin/install-wp-tests.sh wordpress_test root '' localhost $WP_VERSION
+#composer global require "phpunit/phpunit=4.8.*|5.7.*"
+#bash bin/install-wp-tests.sh wordpress_test root '' localhost $WP_VERSION
 phpunit --version
 php -v
-phpunit  --configuration phpunit.xml --coverage-text || exit 1
-WP_MULTISITE=1 phpunit  --configuration phpunit.xml --coverage-text || exit 1
+NO_COVERAGE=$([ -z $CC_TEST_REPORTER_ID ] && echo " --no-coverage" || echo "")
+
+phpunit  --configuration phpunit.xml --coverage-text $NO_COVERAGE || exit 1
+WP_MULTISITE=1 phpunit  --configuration phpunit.xml --coverage-text $NO_COVERAGE || exit 1
