af6af4830eeb1043e9b9b4f7126fb94a4b504ad3
selul <marius.cristea@vertistudio.com>
2019-01-07 18:15:08 +0200

    build: fix cc report

--- bin/run-unit-tests.sh
+++ bin/run-unit-tests.sh
@@ -3,5 +3,5 @@ composer global require "phpunit/phpunit=4.8.*|5.7.*"
 bash bin/install-wp-tests.sh wordpress_test root '' localhost $WP_VERSION
 phpunit --version
 php -v
-phpunit || exit 1
-WP_MULTISITE=1 phpunit || exit 1
+phpunit  --configuration phpunit.xml --coverage-text || exit 1
+WP_MULTISITE=1 phpunit  --configuration phpunit.xml --coverage-text || exit 1
