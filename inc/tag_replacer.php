ab1cf528c554b37d51dea4e24edcb2d0a3434ed1
Bogdan Preda <bogdan.preda@themeisle.com>
2019-01-14 13:11:06 +0200

    chore: testing

--- inc/tag_replacer.php
+++ inc/tag_replacer.php
@@ -64,7 +64,6 @@ final class Optml_Tag_Replacer extends Optml_App_Replacer {
 	 */
 	public function process_image_tags( $content, $images = array() ) {
 		$image_sizes = self::image_sizes();
-		var_dump( $images );
 		foreach ( $images[0] as $index => $tag ) {
 			$width   = $height = false;
 			$resize  = array();
