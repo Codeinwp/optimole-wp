38e6b55bc99b14be32ddb89cbf36f9b4b98f46c5
Bogdan Preda <bogdan.preda@themeisle.com>
2019-01-14 17:02:43 +0200

    chore: testing

--- inc/image.php
+++ inc/image.php
@@ -128,7 +128,7 @@ class Optml_Image {
 			$path = sprintf( '/%s%s', $this->get_signature( $path ), $path );
 		}
 
-		rtrim( $path, '%5C' );
+		$path = rtrim( $path, '%5C' );
 
 		return sprintf( '%s%s', Optml_Config::$service_url, $path );
 
