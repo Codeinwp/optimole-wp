75909c2515ba788a158b0eb00934c10725f23821
Bogdan Preda <bogdan.preda@themeisle.com>
2019-01-14 14:05:57 +0200

    chore: testing

--- inc/lazyload_replacer.php
+++ inc/lazyload_replacer.php
@@ -54,7 +54,7 @@ final class Optml_Lazyload_Replacer extends Optml_App_Replacer {
 		}
 
 		var_dump( $original_url );
-		$low_url = str_replace( '/q:auto/', '/q:eco/', $original_url );
+		$low_url = str_replace( '/q:auto/', '/q:eco/', $new_url );
 		var_dump( $low_url );
 		$no_script_tag = str_replace(
 			array(
