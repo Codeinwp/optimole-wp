b33dbcacad30c893d4bdbeb8760d60ad3ad48620
selul <marius.cristea@vertistudio.com>
2019-02-13 12:34:18 +0200

    build: update npm dependencies

--- assets/vue/components/api-key-form.vue
+++ assets/vue/components/api-key-form.vue
@@ -4,17 +4,17 @@
 			<label v-if="isConnected"
 					class="label api-key-label has-text-grey-dark">{{strings.api_key_placeholder}}:</label>
 			<div class="control is-expanded api-key-control">
-				<input :type="isConnected ? 'password' : 'text'" :disabled="isConnected" name="api_key" class="input is-small"
+				<input :type="isConnected ? 'password' : 'text'" :disabled="isConnected" name="api_key" class="input  "
 						:class="validKey ? '' : 'is-danger'" :placeholder="strings.api_key_placeholder"
 						v-model="apiKey">
 			</div>
 			<div class="control">
-				<button v-if="! isConnected" class="button button is-success is-small"
+				<button v-if="! isConnected" class="button button is-success  "
 						@click="connect" :class="{ 'is-loading' : this.$store.state.isConnecting }">
 					<span class="icon"><i class="dashicons dashicons-admin-plugins"></i></span>
 					<span>{{strings.connect_btn}}</span>
 				</button>
-				<button v-else class="button is-danger is-small" @click="disconnect"
+				<button v-else class="button is-danger " @click="disconnect"
 						:class="{ 'is-loading' : this.$store.state.isConnecting }">
 					<span class="icon"><i class="dashicons dashicons-dismiss"></i></span>
 					<span>{{strings.disconnect_btn}}</span>
