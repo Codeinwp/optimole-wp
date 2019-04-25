3a8c46b284a49d7daf28bbfcb35554d384d9c36a
selul <marius.cristea@vertistudio.com>
2019-04-25 10:05:38 +0300

    fix: remove image replacement on Thrive Architect editor

--- inc/manager.php
+++ inc/manager.php
@@ -141,6 +141,9 @@ final class Optml_Manager {
 		if ( array_key_exists( 'ct_builder', $_GET ) && $_GET['ct_builder'] == 'true' ) {
 			return false; // @codeCoverageIgnore
 		}
+		if ( array_key_exists( 'tve', $_GET ) && $_GET['tve'] == 'true' ) {
+			return false; // @codeCoverageIgnore
+		}
 		if ( class_exists( 'FLBuilderModel', false ) ) {
 			$post_data = FLBuilderModel::get_post_data();
 			if ( isset( $_GET['fl_builder'] ) || isset( $post_data['fl_builder'] ) ) {
