3488b718c453f8e0339c96da0ade6c0423b8bbfc
Grigore Mihai <grigoremihai56@gmail.com>
2019-08-26 16:43:21 +0300

    chore: added some checks in gif test

--- cypress/integration/test_gif.js
+++ cypress/integration/test_gif.js
@@ -9,6 +9,7 @@ describe( 'Check gif page', function () {
 		cy.get( 'video' ).should( 'have.attr', 'playsinline' ).and( 'contain', '' );
 		cy.get( 'video' ).should( 'have.attr', 'poster' ).and( 'contain', 'i.optimole.com' );
 		cy.get( 'video' ).should( 'have.attr', 'poster' ).and( 'contain', 'f:png' );
+		cy.get( 'video' ).should( 'have.attr', 'original-src' ).and( 'contain', 'i.optimole.com' );
 		cy.get( 'video > source' ).should( 'have.attr', 'src' ).and( 'contain', 'f:mp4' );
 		cy.get( 'video > source' ).should( 'have.attr', 'src' ).and( 'contain', 'i.optimole.com' );
 		cy.get( 'video > source' ).should( 'have.attr', 'type' ).and( 'contain', 'video/mp4' );
