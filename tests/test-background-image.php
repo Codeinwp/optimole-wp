9c60d5e6b49a691f6e62f2ed3e935aa47a5a9da5
Grigore Mihai <grigoremihai56@gmail.com>
2019-08-29 10:09:41 +0300

    chore: modified background-image test

--- tests/test-background-image.php
+++ tests/test-background-image.php
@@ -10,7 +10,8 @@
 class Test_Background_Image extends WP_UnitTestCase
 {
 
-    const IMG_TAGS_GIF = '<div data-hash="5c2602bbe76e6f966a846fb661b9d76d" data-desktop="//366b4e08.ngrok.io/wp-content/uploads/2019/08/image3-10-5.jpg" class="n2-ss-slide-background-image" data-blur="0" style="background-image:url("//example.org/wp-content/themes/test/assets/images/header-300x300.png");">  </div>';
+    const BACKGROUND_IMG = '<div data-hash="5c2602bbe76e6f966a846fb661b9d76d" data-desktop="//366b4e08.ngrok.io/wp-content/uploads/2019/08/image3-10-5.jpg" class="n2-ss-slide-background-image" data-blur="0" style="background-image:url("//example.org/wp-content/themes/test/assets/images/header-300x300.png");">  </div>';
+    const BACKGROUND_IMG_SECOND = '<div data-hash="5c2602bbe76e6f966a846fb661b9d76d" data-desktop="://www.example.org/wp-content/uploads/2018/05/brands2.png?test=123"" class="n2-ss-slide-background-image" data-blur="0" style="background-image:url("//example.org/wp-content/themes/test/assets/images/header-300x300.png");">  </div>';
 
 
     public static $sample_post;
@@ -36,7 +37,7 @@ class Test_Background_Image extends WP_UnitTestCase
 
         self::$sample_post = self::factory()->post->create([
                 'post_title' => 'Test post',
-                'post_content' => self::IMG_TAGS_GIF
+                'post_content' => self::BACKGROUND_IMG
             ]
         );
     }
@@ -44,7 +45,9 @@ class Test_Background_Image extends WP_UnitTestCase
     public function test_should_replace_tag()
     {
 
-        $replaced_content = Optml_Manager::instance()->replace_content(self::IMG_TAGS_GIF);
+        $replaced_content = Optml_Manager::instance()->replace_content(self::BACKGROUND_IMG);
+        $this->assertContains('i.optimole.com', $replaced_content);
+        $replaced_content = Optml_Manager::instance()->replace_content(self::BACKGROUND_IMG_SECOND);
         $this->assertContains('i.optimole.com', $replaced_content);
     }
 }
