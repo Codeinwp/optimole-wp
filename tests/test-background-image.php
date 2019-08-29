1577fcfd1218259dc1875434ae737d72f37ba223
Grigore Mihai <grigoremihai56@gmail.com>
2019-08-29 10:15:48 +0300

    chore: modified background-image test

--- tests/test-background-image.php
+++ tests/test-background-image.php
@@ -11,7 +11,7 @@ class Test_Background_Image extends WP_UnitTestCase
 {
 
     const BACKGROUND_IMG = '<div data-hash="5c2602bbe76e6f966a846fb661b9d76d" data-desktop="//366b4e08.ngrok.io/wp-content/uploads/2019/08/image3-10-5.jpg" class="n2-ss-slide-background-image" data-blur="0" style="background-image:url("//example.org/wp-content/themes/test/assets/images/header-300x300.png");">  </div>';
-    const BACKGROUND_IMG_SECOND = '<div data-hash="5c2602bbe76e6f966a846fb661b9d76d" data-desktop="://www.example.org/wp-content/uploads/2018/05/brands2.png?test=123"" class="n2-ss-slide-background-image" data-blur="0" style="background-image:url("//example.org/wp-content/themes/test/assets/images/header-300x300.png");">  </div>';
+    const BACKGROUND_IMG_SECOND = '<div data-hash="5c2602bbe76e6f966a846fb661b9d76d" data-desktop="://www.example.org/wp-content/uploads/2018/05/brands2.png?test=whatever7_whatever"" class="n2-ss-slide-background-image" data-blur="0" style="background-image:url("//example.org/wp-content/themes/test/assets/images/header-300x300.png");">  </div>';
 
 
     public static $sample_post;
@@ -49,6 +49,7 @@ class Test_Background_Image extends WP_UnitTestCase
         $this->assertContains('i.optimole.com', $replaced_content);
         $replaced_content = Optml_Manager::instance()->replace_content(self::BACKGROUND_IMG_SECOND);
         $this->assertContains('i.optimole.com', $replaced_content);
+        $this->assertNotContains('?test=whatever7_whatever', $replaced_content);
     }
 }
 
