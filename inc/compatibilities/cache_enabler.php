0ba42dd62002de457079444b39ab3d5bf499da77
Grigore Mihai <grigoremihai56@gmail.com>
2019-09-17 18:22:49 +0300

    chore: code style changes

--- inc/compatibilities/cache_enabler.php
+++ inc/compatibilities/cache_enabler.php
@@ -8,22 +8,22 @@
 class Optml_cache_enabler extends Optml_compatibility {
 
 
-    /**
-     * Should we load the integration logic.
-     *
-     * @return bool Should we load.
-     */
-    function should_load() {
-        include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+	/**
+	 * Should we load the integration logic.
+	 *
+	 * @return bool Should we load.
+	 */
+	function should_load() {
+		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
-        return is_plugin_active( 'cache-enabler/cache-enabler.php' );
-    }
+		return is_plugin_active( 'cache-enabler/cache-enabler.php' );
+	}
 
-    /**
-     * Register integration details.
-     */
-    public function register() {
-        add_filter( 'cache_enabler_before_store', [ Optml_Main::instance()->manager, 'replace_content' ], PHP_INT_MAX, 1 );
-    }
+	/**
+	 * Register integration details.
+	 */
+	public function register() {
+		add_filter( 'cache_enabler_before_store', [ Optml_Main::instance()->manager, 'replace_content' ], PHP_INT_MAX, 1 );
+	}
 
 }
