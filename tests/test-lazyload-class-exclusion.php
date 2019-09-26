df681c282a80aa7b4d4017d8a3c7cc1ddcef7570
Grigore Mihai <grigoremihai56@gmail.com>
2019-09-26 16:22:59 +0300

    chore: added some tests

--- tests/test-lazyload-class-exclusion.php
+++ tests/test-lazyload-class-exclusion.php
@@ -46,15 +46,24 @@ class Test_Lazyload_Class_Exclusion extends WP_UnitTestCase
 	public function test_class_exclusions()
 	{
 		$content = '<div>
-          <img class="something another test whatever" src="http://example.org/wp-content/uploads/2019/09/Screenshot.png" alt=""/>;    
-          <img class="test" src="http://example.org/wp-content/uploads/2019/09/img.jpg" alt=""/>;      
-          <img class="testing" src="http://example.org/img.png" alt=""/>;
-          
-		</div>';
+						<img class="something another whatever" src="http://example.org/wp-content/uploads/2019/09/Screenshot.png" alt=""/>;    
+						<img class="test" src="http://example.org/wp-content/uploads/2019/09/img.jpg" alt=""/>;      
+						<img class="testing class" src="http://example.org/img.png" alt=""/>;          
+					</div>';
 		$replaced_content = Optml_Manager::instance()->process_images_from_content($content);
-		var_dump($replaced_content);
 		$this->assertEquals( 3, substr_count( $replaced_content, 'i.optimole.com' ) );
 		$this->assertNotContains('data-opt-src', $replaced_content);
-
+	}
+	public function test_class_exclusions_does_not_affect_regular_replacement()
+	{
+		$content = '<div>
+						<img class="something another code" src="http://example.org/wp-content/uploads/2019/09/Screenshot.png" alt=""/>; 
+						<img class="whatever_code_test" src="http://example.org/img.jpg" alt=""/>;          
+					</div>';
+		$replaced_content = Optml_Manager::instance()->process_images_from_content($content);
+		var_dump($replaced_content);
+		$this->assertEquals( 6, substr_count( $replaced_content, 'i.optimole.com' ) );
+		$this->assertEquals( 2, substr_count( $replaced_content, 'data-opt-src' ) );
+		$this->assertEquals( 2, substr_count( $replaced_content, '<noscript>' ) );
 	}
 }
