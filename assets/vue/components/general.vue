21b83eba4f5dc36dbd5917b44768c806091e368c
Bogdan Preda <bogdan.preda@themeisle.com>
2019-10-08 18:08:30 +0300

    chore: changes for #126

--- assets/vue/components/general.vue
+++ assets/vue/components/general.vue
@@ -19,26 +19,6 @@
             </div>
 
         </div>
-        <!-- GIF replacement toggle button -->
-        <div class="field  columns">
-            <label class="label column has-text-grey-dark">
-                {{strings.enable_gif_replace}}
-
-                <p class="is-italic has-text-weight-normal">
-                    {{strings.gif_replacer_desc}}
-                </p>
-            </label>
-
-            <div class="column is-3 ">
-                <toggle-button :class="'has-text-dark'"
-                               v-model="gifReplacementStatus"
-                               :disabled="this.$store.state.loading"
-                               :labels="{checked: strings.enabled, unchecked: strings.disabled}"
-                               :width="80"
-                               :height="25"
-                               color="#008ec2"></toggle-button>
-            </div>
-        </div>
         <!--Scale and lazy load toggle -->
         <div class="field  is-fullwidth columns" :class="{'is-field-disabled':isReplacerOff }">
             <label class="label column has-text-grey-dark">
@@ -113,16 +93,6 @@
                     this.isReplacerOff = !value;
                     this.$emit('update-status', value);
                     this.new_data.image_replacer = value ? 'enabled' : 'disabled'
-                }
-            },
-            gifReplacementStatus: {
-                set: function (value) {
-                    this.showSave = true;
-                    this.new_data.img_to_video = value ? 'enabled' : 'disabled';
-                },
-                get: function () {
-                    return !(this.site_settings.img_to_video === 'disabled');
-
                 }
             },
             lazyLoadStatus: {
