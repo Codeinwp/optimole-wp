7b25e795519ab6711a80f544dce1a51eec3534cd
Bogdan Preda <bogdan.preda@themeisle.com>
2019-10-17 14:44:22 +0300

    [skip release]fix: e2e test

--- cypress/integration/test_gif.js
+++ cypress/integration/test_gif.js
@@ -8,7 +8,7 @@ describe( 'Check gif page', function () {
 		cy.get( 'video' ).should( 'have.attr', 'loop' ).and( 'contain', '' );
 		cy.get( 'video' ).should( 'have.attr', 'playsinline' ).and( 'contain', '' );
 		cy.get( 'video' ).should( 'have.attr', 'poster' ).and( 'contain', 'i.optimole.com' );
-		cy.get( 'video' ).should( 'have.attr', 'poster' ).and( 'contain', 'f:png' );
+		cy.get( 'video' ).should( 'have.attr', 'poster' ).and( 'contain', 'q:eco' );
 		cy.get( 'video' ).should( 'have.attr', 'original-src' ).and( 'contain', 'i.optimole.com' );
 		cy.get( 'video > source' ).should( 'have.attr', 'src' ).and( 'contain', 'f:webm' );
 		cy.get( 'video > source' ).should( 'have.attr', 'src' ).and( 'contain', 'i.optimole.com' );
