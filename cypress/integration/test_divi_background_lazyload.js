1abd454d99cd6e39a79b39c1a3c156978b48d1b7
Grigore Mihai <grigoremihai56@gmail.com>
2019-11-05 14:05:24 +0200

    chore: improved cypress test for divi

--- cypress/integration/test_divi_background_lazyload.js
+++ cypress/integration/test_divi_background_lazyload.js
@@ -1,6 +1,6 @@
 describe( 'Check Divi Background Page', function () {
 	it( 'successfully loads', function () {
-		cy.visit( '/test_divi_background_lazyload/' )
+		cy.visit( '/divi/' )
 	} );
 	//from here
 	it( 'Divi slide that is in view should have optml-bg-lazyloaded', function () {
@@ -14,8 +14,12 @@ describe( 'Check Divi Background Page', function () {
 	} );
 	//to here we test the slider
 	//from here we test the standard divi module where you can have background images
-	it( 'Divi module that is in view should have optml-bg-lazyloaded', function () {
-		cy.get( '.entry-content' ).find( '.et_pb_module' ).eq( 0 ).should( 'have.attr', 'class' ).and( 'include', 'optml-bg-lazyloaded' );
+	//and the row background, section background
+	it( 'Divi section that is in view should have optml-bg-lazyloaded', function () {
+		cy.get( '.entry-content' ).find( '.et_pb_with_background' ).eq( 0 ).should( 'have.attr', 'class' ).and( 'include', 'optml-bg-lazyloaded' );
+	} );
+	it( 'Divi row that is in view should have optml-bg-lazyloaded', function () {
+		cy.get( '.entry-content' ).find( '.et_pb_row_0' ).eq( 0 ).should( 'have.attr', 'class' ).and( 'include', 'optml-bg-lazyloaded' );
 	} );
 	it( 'Divi module that is in view should have optml-bg-lazyloaded', function () {
 		cy.get( '.entry-content' ).find( '.et_pb_module' ).eq( 1 ).should( 'have.attr', 'class' ).and( 'include', 'optml-bg-lazyloaded' );
@@ -23,10 +27,19 @@ describe( 'Check Divi Background Page', function () {
 	it( 'Divi module that is in view should have optml-bg-lazyloaded', function () {
 		cy.get( '.entry-content' ).find( '.et_pb_module' ).eq( 2 ).should( 'have.attr', 'class' ).and( 'include', 'optml-bg-lazyloaded' );
 	} );
-	it( 'Divi module that is in view should have optml-bg-lazyloaded', function () {
-		cy.get( '.entry-content' ).find( '.et_pb_module' ).eq( 3 ).should( 'have.attr', 'class' ).and( 'include', 'optml-bg-lazyloaded' );
+	it( 'Divi slide that is not in view should have optml-bg-lazyloaded', function () {
+		cy.get( '.entry-content' ).find( '.et_pb_slides > .et_pb_slide_3' ).eq( 0 ).should( 'have.css', 'background-image' ).and( 'match', /none/ );
+	} );
+	it( 'Divi slide that is not in view should have optml-bg-lazyloaded', function () {
+		cy.get( '.entry-content' ).find( '.et_pb_slides > .et_pb_slide_4' ).eq( 0 ).should( 'have.css', 'background-image' ).and( 'match', /none/ );
+	} );
+	it( 'Divi row that is not in view should have background', function () {
+		cy.get( '.entry-content' ).find( '.et_pb_module' ).eq( 4 ).should( 'have.css', 'background-image' ).and( 'match', /none/ );
 	} );
 	it( 'Divi module that is not in view should have background', function () {
-		cy.get( '.entry-content' ).find( '.et_pb_module' ).eq( 4 ).should( 'have.css', 'background-image' ).and( 'match', /none/ );
+		cy.get( '.entry-content' ).find( '.et_pb_row_3' ).eq( 0 ).should( 'have.css', 'background-image' ).and( 'match', /none/ );
+	} );
+	it( 'Divi module that is not in view should have background', function () {
+		cy.get( '.entry-content' ).find( '.et_pb_with_background' ).eq( 1 ).should( 'have.css', 'background-image' ).and( 'match', /none/ );
 	} );
 } );
