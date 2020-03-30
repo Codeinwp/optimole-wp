110cba3a7ebaf8dbcdfdfe41fd7072a7f554a225
Grigore Mihai <grigoremihai56@gmail.com>
2020-03-30 13:39:10 +0300

    chore: modifed cypress test

--- cypress/integration/test_shareaholic_pinterest.js
+++ cypress/integration/test_shareaholic_pinterest.js
@@ -6,12 +6,10 @@ describe( 'Shareaholic', function () {
 		cy.get( 'li.shareaholic-share-button[data-service=\"pinterest\"]' ).click( { multiple: true, force: true  } )
 	} );
 	it( 'images should not have quality:eco', function () {
-		cy.get( 'img' ).should( ( $imgs ) => {
-			expect( $imgs ).to.have.length( 5 )
-			expect( $imgs.eq( 0 ) ).to.have.attr( 'src' ).and.to.not.contain( 'eco' )
-			expect( $imgs.eq( 1 ) ).to.have.attr( 'src' ).and.to.not.contain( 'eco' )
-			expect( $imgs.eq( 2 ) ).to.have.attr( 'src' ).and.to.not.contain( 'eco' )
-			expect( $imgs.eq( 3 ) ).to.have.attr( 'src' ).and.to.not.contain( 'eco' )
+		cy.get( 'img' ).each( ( $el, index, $list ) => {
+			cy.log( $el );
+			cy.log( $el.attr( 'src' ) );
+			expect( $el ).to.have.attr( 'src' ).and.to.not.contain( 'eco' );
 		} );
 	} );
 } );
