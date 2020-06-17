cd2a757f99c1f1da4383b6366acb2fbc22205ab0
Grigore Mihai <grigoremihai56@gmail.com>
2020-06-17 12:15:22 +0300

    chore: small changes

--- inc/compatibilities/wp_fastest_cache.php
+++ inc/compatibilities/wp_fastest_cache.php
@@ -15,22 +15,14 @@ class Optml_wp_fastest_cache extends Optml_compatibility {
 	 */
 	function should_load() {
 		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-		$settings = new Optml_Settings();
-		return $settings->get( 'cdn' ) === 'enabled' && is_plugin_active( 'wp-fastest-cache/wpFastestCache.php' );
+		return Optml_Main::instance()->admin->settings->get( 'cdn' ) === 'enabled' && is_plugin_active( 'wp-fastest-cache/wpFastestCache.php' );
 	}
 
 	/**
 	 * Register integration details.
 	 */
 	public function register() {
-		add_filter( 'wpfc_buffer_callback_filter', [$this, 'optml_process_content'], 10, 2 );
-	}
-	/**
-	 * Process the buffer befor caching.
-	 */
-	public function optml_process_content( $buffer ) {
-		$buffer = Optml_Manager::instance()->replace_content( $buffer );
-		return $buffer;
+		add_filter( 'wpfc_buffer_callback_filter', [ Optml_Main::instance()->manager, 'replace_content' ], 10, 2 );
 	}
 }
 
