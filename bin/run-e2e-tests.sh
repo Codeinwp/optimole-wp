2edefbd935419bf20f0dfe04ba2ddc3992da04a4
selul <marius.cristea@vertistudio.com>
2021-02-09 18:37:28 +0200

    fix staging deploy

--- bin/run-e2e-tests.sh
+++ bin/run-e2e-tests.sh
@@ -10,5 +10,5 @@ mkdir $HOME/.ssh
 echo "$SSH_KEY" > "$HOME/.ssh/key"
 chmod 600 "$HOME/.ssh/key"
 
-rsync --delete-before -rc --force --exclude-from="$GITHUB_WORKSPACE/.distignore" -e "ssh -i $HOME/.ssh/key -o StrictHostKeyChecking=no -p $SSH_PORT" "$GITHUB_WORKSPACE/" $SSH_USERNAME@$SSH_HOST:$SSH_PATH
+rsync --delete -rc --force --exclude-from="$GITHUB_WORKSPACE/.distignore" -e "ssh -i $HOME/.ssh/key -o StrictHostKeyChecking=no -p $SSH_PORT" "$GITHUB_WORKSPACE/dist/optimole-wp/" $SSH_USERNAME@$SSH_HOST:$SSH_PATH
 ssh -i $HOME/.ssh/key -o StrictHostKeyChecking=no -p $SSH_PORT $SSH_USERNAME@$SSH_HOST "cd /var/www && docker-compose run --rm cli wp elementor flush_css --url=http://testing.optimole.com"
