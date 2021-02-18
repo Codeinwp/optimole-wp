96446755c9855bd0d1f59383d3552a85160b6edd
Augusto Silva <augusto@themeisle.com>
2021-02-17 22:32:57 -0300

    chore: install libgbm dependency for Cypress

---
+++ bin/cypress-docker-dependencies.sh
@@ -0,0 +1,13 @@
+sudo apt-get update && \
+  sudo apt-get install --no-install-recommends -y \
+  libgtk2.0-0 \
+  libgtk-3-0 \
+  libnotify-dev \
+  libgconf-2-4 \
+  libgbm-dev \
+  libnss3 \
+  libxss1 \
+  libasound2 \
+  libxtst6 \
+  xauth \
+  xvfb \
