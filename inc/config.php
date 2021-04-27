5f3c309775ac6047b02b262cc10c57b7e26267ab
GrigoreMihai <grigoremihai56@gmail.com>
2021-04-27 19:39:41 +0300

    chore: added latin unicode characters to match url

--- inc/config.php
+++ inc/config.php
@@ -51,7 +51,7 @@ class Optml_Config {
 	 * @var string
 	 */
 
-	public static $chars = '\/:,~\\\\.\-\–\d_@%A-Za-z\p{L}\p{M}\p{N}';
+	public static $chars = '\/:,~\\\\.\-\–\d_@%A-Za-z\p{L}\p{M}\p{N}\x{0080}-\x{017F}';
 	/**
 	 * Service api key.
 	 *
