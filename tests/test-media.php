e1421e2c69aa65997f050ffa48a0fb8cca2b419c
GrigoreMihai <grigoremihai56@gmail.com>
2022-09-01 18:14:28 +0300

    chore: updated tests

--- tests/test-media.php
+++ tests/test-media.php
@@ -189,7 +189,7 @@ class Test_Media extends WP_UnitTestCase {
 		$my_size_image = wp_get_attachment_image_src(self::$sample_attachement, 'my_size_crop' );
 
 		$this->assertContains( 'example.i.optimole.com', $my_size_image[0] );
-		$this->assertContains('/cb:nLJ0vstD/w:200/h:200/q:mauto/rt:fill/g:nowe/process:' . self::$sample_attachement .'/id:579c7f7707ce87caa65fdf50c238a117/http://example.org/1.jpg', $my_size_image[0]);
+		$this->assertContains('/cb:vthvq0gK/w:200/h:200/q:mauto/rt:fill/g:nowe/process:' . self::$sample_attachement .'/id:579c7f7707ce87caa65fdf50c238a117/http://example.org/1.jpg', $my_size_image[0]);
 
 		$this->assertContains( 'example.i.optimole.com', $image_thumbnail_size[0] );
 		$this->assertContains( '/cb:vthvq0gK/w:150/h:150/q:mauto/rt:fill/g:ce/process:' . self::$sample_attachement .'/id:579c7f7707ce87caa65fdf50c238a117/http://example.org/1.jpg', $image_thumbnail_size[0] );
@@ -218,7 +218,7 @@ class Test_Media extends WP_UnitTestCase {
 		$image_thumbnail_size = wp_get_attachment_image_src(self::$sample_attachement, 'thumbnail');
 
 		$this->assertContains( 'example.i.optimole.com', $image_thumbnail_size[0] );
-		$this->assertContains( '/cb:nLJ0vstD/w:150/h:150/q:mauto/rt:fill/g:ce/process:' . self::$sample_attachement .'/id:579c7f7707ce87caa65fdf50c238a117/http://example.org/1.jpg', $image_thumbnail_size[0] );
+		$this->assertContains( '/cb:vthvq0gK/w:150/h:150/q:mauto/rt:fill/g:ce/process:' . self::$sample_attachement .'/id:579c7f7707ce87caa65fdf50c238a117/http://example.org/1.jpg', $image_thumbnail_size[0] );
 
 		$this->assertContains( 'example.i.optimole.com', $image_medium_size );
 		$this->assertContains( '/cb:vthvq0gK/w:300/h:200/q:mauto/process:' . self::$sample_attachement .'/id:579c7f7707ce87caa65fdf50c238a117', $image_medium_size[0] );
