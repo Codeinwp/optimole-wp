23d065d93a05d0c1257c501e8bbe00f3d7fd3ab3
Hardeep Asrani <hardeepasrani@gmail.com>
2023-04-03 23:59:06 +0530

    Add Unit tests

--- tests/test-media.php
+++ tests/test-media.php
@@ -255,4 +255,13 @@ class Test_Media extends WP_UnitTestCase {
 
 	}
 
+	/**
+	 * Test images with special characters.
+	 */
+	public function test_special_characters_upload() : void {
+		$special_character_attachment = self::factory()->attachment->create_upload_object( OPTML_PATH . 'tests/assets/special-characters-•⋿∀.jpg' );
+		$content = wp_get_attachment_image( $special_character_attachment );
+		$this->assertEquals(  "<img width=\"150\" height=\"150\" src=\"https://example.i.optimole.com/w:150/h:150/q:mauto/rt:fill/g:ce/process:71/id:579c7f7707ce87caa65fdf50c238a117/http://example.org/special-characters-•⋿∀.jpg\" class=\"attachment-thumbnail size-thumbnail\" alt=\"\" decoding=\"async\" />", $content );
+	}
+
 }
