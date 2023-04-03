c84291c0d888c40cb107902f9b7c47892bdb064e
Hardeep Asrani <hardeepasrani@gmail.com>
2023-04-04 01:58:10 +0530

    Add Unit tests

--- tests/test-replacer.php
+++ tests/test-replacer.php
@@ -308,9 +308,12 @@ class Test_Replacer extends WP_UnitTestCase {
 		$content          = '
 		https://www.example.org/wp-content/uploads/2018/05/@brands.png
 		https://www.example.org/wp-content/uploads/2018/05/%brands.png
+		https://www.example.org/wp-content/uploads/2018/05/•brands.png
+		https://www.example.org/wp-content/uploads/2018/05/⋿brands.png
+		https://www.example.org/wp-content/uploads/2018/05/∀brands.png
 		';
 		$replaced_content = Optml_Manager::instance()->replace_content( $content );
-		$this->assertEquals( 2, substr_count( $replaced_content, 'i.optimole.com' ) );
+		$this->assertEquals( 5, substr_count( $replaced_content, 'i.optimole.com' ) );
 
 	}
 
