8915e6361c0cf1a1ed794551bbead8cc8057cdf6
Hardeep Asrani <hardeepasrani@gmail.com>
2023-05-02 00:37:22 +0530

    wip

--- cypress/e2e/test_otter_background_lazyload.js
+++ cypress/e2e/test_otter_background_lazyload.js
@@ -2,16 +2,30 @@ describe("Check Otter Background Lazyload", function () {
     it("successfully loads", function () {
       cy.visit("/otter/background-lazyload/");
     });
-    it("Otter Flip block should have background lazyloaded", function () {
+    it("Otter Flip block front should have background lazyloaded", function () {
       cy.get(".wp-block-themeisle-blocks-flip")
-        .find(".o-flip-content")
+        .find(".o-flip-front")
         .eq(0)
         .should("have.attr", "class")
         .and("include", "optml-bg-lazyloaded");
     });
-    it("Otter Flip block should have background image url optimized (ie. external css is processed)", function () {
+    it("Otter Flip block front should have background image url optimized (ie. external css is processed)", function () {
         cy.get(".wp-block-themeisle-blocks-flip")
-        .find(".o-flip-content")
+        .find(".o-flip-front")
+        .eq(0)
+        .should("have.css", "background-image")
+        .and("match", /url\(.*\.i\.optimole\.com.*\)/);
+    });
+    it("Otter Flip block back should have background lazyloaded", function () {
+      cy.get(".wp-block-themeisle-blocks-flip")
+        .find(".o-flip-back")
+        .eq(0)
+        .should("have.attr", "class")
+        .and("include", "optml-bg-lazyloaded");
+    });
+    it("Otter Flip block back should have background image url optimized (ie. external css is processed)", function () {
+        cy.get(".wp-block-themeisle-blocks-flip")
+        .find(".o-flip-back")
         .eq(0)
         .should("have.css", "background-image")
         .and("match", /url\(.*\.i\.optimole\.com.*\)/);
