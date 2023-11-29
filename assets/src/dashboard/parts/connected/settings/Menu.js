582a51b109cf99c9073f3da8587c477f7ef66c11
abaicus <baicusandrei@me.com>
2023-11-29 13:16:14 +0200

    fix: dashboard menu accessibility

--- assets/src/dashboard/parts/connected/settings/Menu.js
+++ assets/src/dashboard/parts/connected/settings/Menu.js
@@ -58,24 +58,24 @@ const SubMenu = ({
 	setTab
 }) => {
 	return (
-		<ul className="flex flex-col gap-1 m-0 mt-3">
-			{ children.map( item => (
-				<li
-					key={ item.value }
-					className={ classnames(
-						'not-italic font-normal text-base text-purple-gray cursor-pointer rounded px-4 py-2 m-0 hover:text-info',
-						{
-							'bg-light-blue hover:text-purple-gray': tab === item.value
-						}
-					) }
-					onClick={ e => {
-						e.stopPropagation();
-						setTab( item.value );
-					} }
-				>
-					{ item.label }
-				</li>
-			) ) }
+		<ul className="flex flex-col gap-1 m-0 mt-1">
+			{children.map( item => {
+				const { value, label } = item;
+				const classes = classnames(
+					{
+						'bg-light-blue hover:text-purple-gray': tab === value,
+						'bg-transparent hover:text-info': tab !== value
+					},
+					'w-full text-left border-0 font-normal text-base text-purple-gray cursor-pointer rounded px-4 py-2 m-0 hover:text-info'
+				);
+				return (
+					<li key={value}>
+						<button className={classes} onClick={() => setTab( value )}>
+							{label}
+						</button>
+					</li>
+				);
+			})}
 		</ul>
 	);
 };
@@ -86,42 +86,36 @@ const Menu = ({
 }) => {
 	return (
 		<div className="basis-1/5">
-			<ul className="flex flex-col m-0 gap-3">
-				{ menuItems.map( item => {
+			<ul className="grid m-0 gap-1">
+				{menuItems.map( item => {
 					const isActive = tab === item.value || ( item.children && item.children.some( child => child.value === tab ) );
-
+					const buttonClasses = classnames({ '!text-info': isActive }, 'w-full bg-transparent border-0 flex items-center appearance-none not-italic font-semibold text-base text-purple-gray cursor-pointer hover:text-info py-2' );
 					return (
-						<li
-							key={ item.value }
-							className={ classnames(
-								'not-italic font-semibold text-base text-purple-gray cursor-pointer hover:text-info',
-								{
-									'!text-info': isActive
-								}
-							) }
-							onClick={ () => setTab( item.value ) }
-						>
-							<div className="flex items-center">
-								{ item.label }
-
-								{ item.children && (
+						<li key={item.value} className='m-0'>
+							<button
+								className={buttonClasses}
+								onClick={() => setTab( item.value )}
+							>
+								{item.label}
+								{item.children && (
 									<Icon
-										icon={ isActive ? chevronUp : chevronDown }
-										className="inline-block ml-2"
+										icon={isActive ? chevronUp : chevronDown}
+										className="inline-block ml-2 fill-current"
 									/>
-								) }
-							</div>
+								)}
+							</button>
 
-							{ ( item.children && isActive ) && (
+
+							{( item.children && isActive ) && (
 								<SubMenu
-									children={ item.children }
-									tab={ tab }
-									setTab={ setTab }
+									children={item.children}
+									tab={tab}
+									setTab={setTab}
 								/>
-							) }
+							)}
 						</li>
 					);
-				}) }
+				})}
 			</ul>
 		</div>
 	);
