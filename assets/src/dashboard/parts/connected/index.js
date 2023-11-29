8127c6437e221a61940dbcbe481e70f2b1fd285b
selul <marius.cristea@vertistudio.com>
2023-11-29 16:00:18 +0200

    add final tweaks

--- assets/src/dashboard/parts/connected/index.js
+++ assets/src/dashboard/parts/connected/index.js
@@ -26,7 +26,7 @@ if ( 'undefined' !== typeof window && optimoleDashboardApp.user_data.plan ) {
 	formbricks.init({
 		environmentId: 'clo8wxwzj44orpm0gjchurujm',
 		apiHost: 'https://app.formbricks.com',
-		userId: 'opt_' + ( optimoleDashboardApp.user_data.id  + 74373  ),
+		userId: 'opt_' + ( optimoleDashboardApp.user_data.id ),
 		debug:true
 	});
 	formbricks.setAttribute( 'plan', optimoleDashboardApp.user_data.plan );
