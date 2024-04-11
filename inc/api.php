3761fd04be9f2f6bcbaaa26e4afaf32dfb62f95d
selul <marius.cristea@vertistudio.com>
2024-04-11 18:12:32 +0300

    add translators comments and fix strings

--- inc/api.php
+++ inc/api.php
@@ -198,7 +198,7 @@ final class Optml_Api {
 
 		if ( intval( $response['code'] ) !== 200 ) {
 			if ( isset( $response['error'] ) && $response['error'] === 'domain_not_accessible' ) {
-				return new WP_Error( 'domain_not_accessible', sprintf( __( 'It seems Optimole is having trouble reaching your website. This issue often occurs if your website is private, local, or protected by a firewall. But don\'t stress – it\'s an easy fix! Ensure your website is live and accessible to the public. If a firewall is in place, just tweak the settings to allow the %1$sOptimole(1.0)%2$s user agent access to your website. %3$sLearn More%4$s', 'optimole-wp' ), '<i>', '</i>', '<a href="https://docs.optimole.com/article/1976-resolving-optimole-access-to-your-website" target="_blank">', '</a>' ) );
+				return new WP_Error( 'domain_not_accessible', sprintf( /* translators: 1 start of the italic tag, 2 is the end of italic tag,  3 is starting anchor tag, 4 is the ending anchor tag. */ __( 'It seems Optimole is having trouble reaching your website. This issue often occurs if your website is private, local, or protected by a firewall. But don\'t stress – it\'s an easy fix! Ensure your website is live and accessible to the public. If a firewall is in place, just tweak the settings to allow the %1$sOptimole(1.0)%2$s user agent access to your website. %3$sLearn More%4$s', 'optimole-wp' ), '<i>', '</i>', '<a href="https://docs.optimole.com/article/1976-resolving-optimole-access-to-your-website" target="_blank">', '</a>' ) );
 			}
 			if ( $path === 'optml/v2/account/complete_register_remote' && isset( $response['error'] ) ) {
 				if ( strpos( $response['error'], 'This email address is already registered.' ) !== false ) {
--- inc/conflicts/jetpack_lazyload.php
+++ inc/conflicts/jetpack_lazyload.php
@@ -22,7 +22,7 @@ class Optml_Jetpack_Lazyload extends Optml_Abstract_Conflict {
 	 * @access  public
 	 */
 	public function define_message() {
-		$this->message = sprintf( __( 'It seems your are using %1$sJetpack%2$s with Lazy loading option ON. %3$s Optimole already provides a lazy loading mechanism by it\'s own which might conflict with this. If you would like to further use Optimole lazy loading feature, you can turn that off from %4$sJetpack -> Perfomance%5$s page. ', 'optimole-wp' ), '<b>', '</b>', '<br/>', '<a target="_blank" href="' . admin_url( 'admin.php?page=jetpack#/performance' ) . '">', '</a>' );
+		$this->message = sprintf( /* translators: 1 is the start of the bold tag, 2 is ending bold tag, 3 is new line tag, 4 is anchor tag start, 5 is ending anchor tag */__( 'It seems your are using %1$sJetpack%2$s with Lazy loading option ON. %3$s Optimole already provides a lazy loading mechanism by it\'s own which might conflict with this. If you would like to further use Optimole lazy loading feature, you can turn that off from %4$sJetpack -> Perfomance%5$s page. ', 'optimole-wp' ), '<b>', '</b>', '<br/>', '<a target="_blank" href="' . admin_url( 'admin.php?page=jetpack#/performance' ) . '">', '</a>' );
 	}
 
 	/**
