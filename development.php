bb4b6b1646daa0a403be6c82537ab298db4c5aaf
Carl Alexander <carlalexander@gmail.com>
2024-09-27 10:56:19 -0400

    test: fix `development.php` to also return if we run a test as a separate process

--- development.php
+++ development.php
@@ -4,7 +4,15 @@
  *
  * @package ThemeIsle
  */
-if ( defined( 'PHPUNIT_COMPOSER_INSTALL' ) ) {
+
+// This condition tries to detect if we're running PHPUnit. This is complex because the file is loaded via the
+// composer autoloader. You can detect that 99% of the time because PHPUnit uses the constant
+// PHPUNIT_COMPOSER_INSTALL to load the autoloader. That said, if you run a test in a separate process, it won't
+// run that code again and the constant doesn't exist. As a fallback, we use the PHPUNIT_RUNNING environment variable
+// defined in the phpunit.xml to detect if we're in a PHPUnit process. We can't always use the environment variable
+// because by default PHPUnit loads the composer autoloader before it assigns those environment variables. That's
+// why both conditions are needed.
+if ( defined( 'PHPUNIT_COMPOSER_INSTALL' ) || getenv( 'PHPUNIT_RUNNING' ) ) {
 	return;
 }
 
