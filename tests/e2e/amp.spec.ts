9a07b251a15ef3a3bd25afa092eb61accea064d4
abaicus <baicusandrei@me.com>
2025-02-20 20:46:13 +0200

    test: revert failtest

--- tests/e2e/amp.spec.ts
+++ tests/e2e/amp.spec.ts
@@ -8,7 +8,6 @@ test.describe('Check amp page', () => {
   test('AMP body has Optimole no script class', async ({ page }) => {
     const html = page.locator('html');
     await expect(html).not.toHaveClass(/optml_no_js/);
-    await expect(html).toHaveClass(/optml_no_js/);
   });
 
   test('AMP images should have replaced srcs', async ({ page }) => {
