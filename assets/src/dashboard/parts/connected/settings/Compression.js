6d03fb55f01e87046a9d66b8d3f8cb52c7a81cd8
Ionut Neagu <ionut.neagu@vertistudio.com>
2025-10-01 14:21:25 +0200

    fixed linting

--- assets/src/dashboard/parts/connected/settings/Compression.js
+++ assets/src/dashboard/parts/connected/settings/Compression.js
@@ -23,7 +23,7 @@ import {
 
 import { useSelect } from '@wordpress/data';
 
-import { useEffect, useRef } from '@wordpress/element';
+import { useRef } from '@wordpress/element';
 
 /**
  * Internal dependencies.
@@ -95,7 +95,7 @@ const Compression = ({
 		}
 
 		manualQualityRef.current = getQuality( settings.quality );
-	}, [ isAutoQualityEnabled, settings.quality ] );
+	}, [ isAutoQualityEnabled, settings.quality ]);
 	const updateOption = ( option, value ) => {
 		setCanSave( true );
 		const data = { ...settings };
@@ -333,18 +333,18 @@ const Compression = ({
 					<BaseControl
 						help={ ! isAutoQualityEnabled && optimoleDashboardApp.strings.options_strings.quality_desc }
 					>
-					<ToggleControl
-						label={ optimoleDashboardApp.strings.options_strings.quality_title }
-						help={ () => <p dangerouslySetInnerHTML={ { __html: optimoleDashboardApp.strings.options_strings.ml_quality_desc } } /> }
-						checked={ isAutoQualityEnabled }
-						disabled={ isLoading }
-						className={ classnames(
-							{
-								'is-disabled': isLoading
-							}
-						) }
-						onChange={ handleAutoQualityToggle }
-					/>
+						<ToggleControl
+							label={ optimoleDashboardApp.strings.options_strings.quality_title }
+							help={ () => <p dangerouslySetInnerHTML={ { __html: optimoleDashboardApp.strings.options_strings.ml_quality_desc } } /> }
+							checked={ isAutoQualityEnabled }
+							disabled={ isLoading }
+							className={ classnames(
+								{
+									'is-disabled': isLoading
+								}
+							) }
+							onChange={ handleAutoQualityToggle }
+						/>
 					</BaseControl>
 
 					{ ! isAutoQualityEnabled && (
