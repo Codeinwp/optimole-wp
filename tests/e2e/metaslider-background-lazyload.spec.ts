bcf646075aed780f7fc9b004fecdd42fa4c28f22
selul <marius.cristea@vertistudio.com>
2025-10-09 15:02:45 +0300

    fix: adjust scroll position in background lazyload test

--- tests/e2e/metaslider-background-lazyload.spec.ts
+++ tests/e2e/metaslider-background-lazyload.spec.ts
@@ -16,7 +16,7 @@ test.describe('Check Metaslider Background Page', () => {
   });
 
   test('After scroll slider background images not in view should have no background', async ({ page }) => {
-    await page.evaluate(() => window.scrollTo(0, 2500));
+    await page.evaluate(() => window.scrollTo(0, 100));
 
     const slider = page.locator('.entry-content .coin-slider > .coin-slider').nth(1);
     await expect(slider).toHaveCSS('background-image', /none/);
